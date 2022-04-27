287f570e2e0d42fe9c3671c6199a669f7e8ea431
Raphael Salviano <raphael_salviano@outlook.com>
2022-04-27 16:59:35 -0300

    chore: ajustar configurações prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  semi: true,
   singleQuote: true,
-  trailingComma: 'all',
+  quoteProps: 'consistent',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true
 };
